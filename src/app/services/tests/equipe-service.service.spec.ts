c13cd62ee99cc765cc1d558eac604bdadd798820
wtoure <wtoure@asi.fr>
2023-07-06 11:28:03 +0200

    Mise en place de l'architecture du frontend

--- src/app/services/tests/equipe-service.service.spec.ts
+++ src/app/services/tests/equipe-service.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EquipeServiceService } from './equipe-service.service';
+import { EquipeServiceService } from '../equipe-service.service';
 
 describe('EquipeServiceService', () => {
   let service: EquipeServiceService;
