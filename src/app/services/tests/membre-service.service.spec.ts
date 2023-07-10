fbe1224b7718f106e15322d08f722524cb05f54b
wtoure <wtoure@asi.fr>
2023-07-10 14:49:13 +0200

    Test des urls

--- src/app/services/tests/membre-service.service.spec.ts
+++ src/app/services/tests/membre-service.service.spec.ts
@@ -2,8 +2,7 @@ import { TestBed, getTestBed} from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 
 import { MembreServiceService } from '../membre-service.service'
-import { Membre } from 'src/app/models/membre';
-import { Observable, of } from 'rxjs';
+
 
 describe('MembreServiceService', () => {
   let mockService:MembreServiceService;
@@ -31,20 +30,6 @@ describe('MembreServiceService', () => {
   it("Le service doit définir son URL correctement", () => {
     expect(mockService.getPath()).toBe("http://localhost:8080/api/membres");
   });
-  it("Le service doit pouvoir récupérer la liste des membres",()=>{
-    const result = 1
-    let membres! :Membre[];
-    mockService.findAll().subscribe(data=>{
-      membres=data;
-    })
-    const req = httpMock.expectOne({
-      method:'GET',
-      url:mockService.getPath()
-    })
-    req.flush([Membre])
-    expect(membres.length).toEqual(result)
-    
-  })
   afterEach(() => {
     httpMock.verify();
   });
